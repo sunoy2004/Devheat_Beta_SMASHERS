197048981e15d9461d810a01f51e9282995c6b6a
Anisha Sodani <ui22cs07@iiitsurat.ac.in>
2023-10-04 02:38:40 +0530

    not end yet

--- dev_heat.js
+++ dev_heat.js
@@ -1,4 +1,4 @@
-<script>
+{/* <script>
     var modal = document.getElementById('id01');
 
     window.onclick = function(event) {
@@ -6,6 +6,27 @@
         modal.style.display = "none";
     }
 }
-</script>
+</script> */}
 
 // this is a change
+.foot {
+    margin - left: 60px;
+    background: black;
+}
+
+.foot img {
+    height: 100px;
+    width: 200px;
+}
+
+.foot p {
+    font - size: 30px;
+    text - align: left;
+    margin - left: 60px;
+}
+
+.foot1 {
+    position: absolute;
+    right: 73 %;
+    width: 15 %;
+}
